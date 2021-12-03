1244bb3f5aa6bdc3b963db2c2e99c65536532a3a
CodeItQuick <evanontario@hotmail.com>
2021-12-03 07:56:22 -0400

    somewhat working improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Productivity Application</h1>
-    <app-hero-form-reactive></app-hero-form-reactive>`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {}
--- src/app/reactive/hero-form-reactive.spec.ts
+++ src/app/reactive/hero-form-reactive.spec.ts
@@ -1,19 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-import { ValidationErrors } from '@angular/forms';
+import { ReactiveFormsModule, ValidationErrors } from '@angular/forms';
 import { TimeBlockComponent } from './hero-form-reactive.component';
 
 describe('Given we are on the main task page, ', () => {
   beforeAll(() => {});
 
   it('when the component is created then description is empty', () => {
-    const component = new TimeBlockComponent();
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TimeBlockComponent],
+    });
+    const fixture = TestBed.createComponent(TimeBlockComponent);
+    const component = fixture.componentInstance;
 
     expect(component.description).toBeDefined();
-    expect(component.description?.value).toEqual('');
+    expect(component.description?.value).toEqual(undefined);
   });
 
   it('when no value is entered into 8:00 am then an error is displayed', () => {
-    TestBed.configureTestingModule({ declarations: [TimeBlockComponent] });
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TimeBlockComponent],
+    });
     const fixture = TestBed.createComponent(TimeBlockComponent);
     const component = fixture.componentInstance;
 
@@ -21,24 +29,32 @@ describe('Given we are on the main task page, ', () => {
 
     expect(component.description).toBeDefined();
     expect(component.description?.errors).toBeDefined();
-    expect(component.description?.errors).toEqual(null);
+    expect(component.description?.errors).toEqual({
+      minlength: Object({ requiredLength: 4, actualLength: 1 }),
+    });
   });
-  it('when a single letter is entered into 8:00 am then an error is displayed for minlength', () => {
-    TestBed.configureTestingModule({ declarations: [TimeBlockComponent] });
+  it('when a single letter is entered into the block then an error is displayed for minlength', () => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TimeBlockComponent],
+    });
     const fixture = TestBed.createComponent(TimeBlockComponent);
-    const component = fixture.componentInstance;
+    let component = fixture.componentInstance;
 
-    component.description?.setValue('a');
+    component.updateDescription('a');
     fixture.detectChanges();
 
     expect(component.description?.hasError('minlength')).toBeTrue();
   });
-  it('when a single letter is entered into 8:00 am then an error is displayed for minlength', () => {
-    TestBed.configureTestingModule({ declarations: [TimeBlockComponent] });
+  it('when a five letters are entered into the block then no error is displayed for minlength', () => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TimeBlockComponent],
+    });
     const fixture = TestBed.createComponent(TimeBlockComponent);
-    const component = fixture.componentInstance;
+    let component = fixture.componentInstance;
 
-    component.description?.setValue('abcd');
+    component.updateDescription('abcde');
     fixture.detectChanges();
 
     expect(component.description?.hasError('minlength')).toBeFalse();
