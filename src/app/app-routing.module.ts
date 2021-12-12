1bf1d717c57cf0421caea028b3224504cf90c61e
CodeItQuick <evanontario@hotmail.com>
2021-12-12 08:36:14 -0400

    feat: first microservice made in projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TimeBlockComponent } from './productivity-page/time-block/time-block.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ListComponent } from './todo/list/list.component';
-import { ProductivityRoutingModule } from './productivity-page/productivity-page-routing.module';
 
 const routes: Routes = [
   { path: 'todo/list', component: ListComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TimeBlockComponent } from './productivity-page/time-block/time-block.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
-import { TimeSlotComponent } from './productivity-page/time-slot/time-slot.component';
 import { ListComponent } from './todo/list/list.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ProductivityRequestService } from './productivity-page/productivity-request.service';
-import { ProductivityModule } from './productivity-page/productivity-page.module';
-import { ProductivityRoutingModule } from './productivity-page/productivity-page-routing.module';
-import { ListRoutingModule } from './todo/list-routing.module';
+import { ProductivityModule } from 'projects/productivity-application/src/app/app.module';
 
 @NgModule({
   imports: [
@@ -21,8 +16,7 @@ import { ListRoutingModule } from './todo/list-routing.module';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    ProductivityRoutingModule,
-    ListRoutingModule,
+    ProductivityModule,
   ],
   declarations: [AppComponent, AboutComponent, ListComponent],
   bootstrap: [AppComponent],
