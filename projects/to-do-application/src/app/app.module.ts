9c8278011dc6b76de84675da173f60d6c1556cbf
CodeItQuick <evanontario@hotmail.com>
2021-12-12 09:01:48 -0400

    fix: todo list was borked

--- projects/to-do-application/src/app/app.module.ts
+++ projects/to-do-application/src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './todo/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ListComponent],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
